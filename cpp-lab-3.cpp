4af7e1c00edd6c1d638f5a3c81bac643c3a353ca
Lisa <eamazitova@edu.hse.ru>
2023-10-08 12:55:01 +0300

    second

--- cpp-lab-3.cpp
+++ cpp-lab-3.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 using namespace std;
 // функция нахождения суммы n значений массива
-int sumarr(int* a, int n) {
+int funcsumarr(int* a, int n) {
 	int sum = 0;
 	for (int i = 0; i < n; i++) {
 		sum += a[i];
@@ -23,15 +23,6 @@ bool result(int* a, int n, int sum) {
 	bool caseexl = result(a, (n - 1), sum);
 	return caseincl || caseexl;
 }
-// функция для нахождения 1/2 значения типа int с округлением в меньшую сторону
-int del2(int n) {
-	if (n % 2 == 0) {
-		return n / 2;
-	}
-	else {
-		return (n - 1) / 2;
-	}
-}
 int main() {
 	int n;
 	int pr1 = 0;
@@ -40,12 +31,14 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> cki[i];
 	}
-	for (int i = 0; i < del2(sumarr(cki, n)); i++) {
-		if (result(cki, n, (del2(sumarr(cki, n)) - i)) == 1) {
-			pr1 = del2(sumarr(cki, n)) - i;
+	int sumarr = funcsumarr(cki, n);
+	int sumarr2 = funcsumarr(cki, n) / 2;
+	for (int i = 0; i < sumarr2; i++) {
+		if (result(cki, n, sumarr2 - i) == 1) {
+			pr1 = sumarr2 - i;
 			break;
 		}
 	}
-	int pr2 = sumarr(cki, n) - pr1;
+	int pr2 = sumarr - pr1;
 	cout << "Минимальная разница между порциями = " << abs(pr2 - pr1);
 }
